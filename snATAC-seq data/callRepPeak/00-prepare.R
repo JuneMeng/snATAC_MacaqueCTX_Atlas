3c7e6bee0189d019cda4bc8a18cf21c6dd06de05
chenc <94962038+chenc327@users.noreply.github.com>
2025-04-15 11:23:55 +0800

    Update 00-prepare.R

--- snATAC-seq data/callRepPeak/00-prepare.R
+++ snATAC-seq data/callRepPeak/00-prepare.R
@@ -5,11 +5,11 @@ library(parallel)
 library(tidyverse)
 
 
-geneAnnotation <- readRDS('/cluster/share/atac_group/mafas5/chen_ws/macaca_geneAnnotation.Rds')
+geneAnnotation <- readRDS('macaca_geneAnnotation.Rds')
 
 seqnames(BSgenome.Mfascicularis.NCBI.5.0)=gsub("MFA","chr",seqnames(BSgenome.Mfascicularis.NCBI.5.0))
 genomeAnnotation <- createGenomeAnnotation(genome = BSgenome.Mfascicularis.NCBI.5.0)
-blacklist=read.table('/cluster/share/atac_group/mafas5/ref/macFas5.huada.backlist.bed',header = F,stringsAsFactors = FALSE)
+blacklist=read.table('/cluster/ref/macFas5.huada.backlist.bed',header = F,stringsAsFactors = FALSE)
 blacklist=blacklist[,1:3]
 colnames(blacklist)=c("chr","start","end")
 MF_blacklist=makeGRangesFromDataFrame(blacklist,keep.extra.columns = T)
